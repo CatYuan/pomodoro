2ed1900885391406d7e268463a60da84ff16087a
Catherine Yuan <ccyuan2@illinois.edu>
2020-03-08 22:16:18 +0100

    Added basic CSS for composition

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Pomodoro
+The pomodoro technique is a method that can be used for studying, where one alternates between 25 minute intervals of work and 5 minute intervals of rest. The goal of this app is to help implement this study method.
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
@@ -12,5 +15,5 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.<br />
 You will also see any lint errors in the console.
 
-# Pomodoro
-The pomodoro technique is a method that can be used for studying, where one alternates between 25 minute intervals of work and 5 minute intervals of rest. The goal of this app is to help implement this study method.
+## Bugs
+This project is simply a means to practice React, as a result the app is not 100% functional or without bugs. Possible fixes to this app include, dealing with unexpected values (ex. putting letters into the timer, not entering a value into the timer).
--- src/App.js
+++ src/App.js
@@ -3,8 +3,130 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App'>Hello</div>
+    <div className='App'>
+      <TimerDashboard/>
+      <NavBar/>
+    </div>
   );
+}
+
+class NavBar extends React.Component {
+  render() {
+    return(
+      <div className='NavBar'>
+        <h2>Today's Pomodoros</h2>
+        <ol>
+          <li>Learn React - 25 min</li>
+          <li>ReactApp - 25 min</li>
+        </ol>
+      </div>
+    );
+  }
+}
+
+class TimerDashboard extends React.Component {
+  render() {
+    return(
+      <div className='TimerDashboard'>
+        <Header />
+        <TimerHolder 
+          canEdit={false}
+        />
+      </div>
+    );
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return(
+      <div className='Header'>
+        <img 
+          src={require('./images/tomato.png')} 
+          alt='tomato timer'
+        />
+        <h1>Pomodoro</h1>
+      </div>
+    );
+  }
+}
+
+/**
+ * @param bool canEdit - if true 'EditableTimer' is rendered 
+ *        otherwise Timer is rendered
+ */
+class TimerHolder extends React.Component {
+  render() {
+    if (this.props.canEdit) {
+      return(
+        <EditableTimer/>
+      );
+    } else {
+      return (
+        <Timer
+          title='ReactApp'
+          time='60'
+        />
+      );
+    }
+  }
+}
+
+class EditableTimer extends React.Component {
+  render() {
+    const prompt="What do you want to work on?"
+    return(
+      <div className='EditableTimer'>
+        <label>Project</label>
+        <input type='text' placeholder={prompt} />
+        <input type='time' defaultValue='00:25'/>
+        <button type='submit'>Start</button>
+      </div>
+    );
+  }
+}
+
+/**
+ * @param string title - the task to do
+ * @param int time - the number of seconds on the timer
+ */
+class Timer extends React.Component {
+  render() {
+    if (this.props.time !== 0) {
+      return(
+        <div className='Timer'>
+          <p>{this.props.title}</p>
+          <h3 className='Countdown'>{this.props.time}</h3>
+        </div>
+      );
+    } else {
+      return(
+        <TimesUp
+          title={this.props.title}
+        />
+      );
+    }
+  }
+}
+
+/**
+ * @param string title - the task to do
+ */
+class TimesUp extends React.Component {
+  render() {
+    var buttonPrompt = "Rest";
+    if (this.props.title === "Rest") {
+      buttonPrompt = "Reset"
+    }
+
+    return (
+      <div className='TimesUp'>
+        <p>{this.props.title}</p>
+        <h3 className='Countdown'>0</h3>
+        <button type='button'>{buttonPrompt}</button>
+      </div>
+    );
+  }
 }
 
 export default App;
